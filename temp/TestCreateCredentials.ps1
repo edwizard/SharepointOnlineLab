98d907e19ad31a822fd016ef65936f8d3726d0eb
edwizard <edgarholm@outlook.com>
2018-01-17 12:22:53 +0000

    Temp#1
    Undone/Finished

--- temp/TestCreateCredentials.ps1
+++
@@ -1,2 +0,0 @@
-﻿# Create user object
-$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
--- temp/TestCreateVirtualMachine.ps1
+++
@@ -1,8 +0,0 @@
-﻿# Create a virtual machine configuration
-$vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize Standard_D1 |
-  Set-AzureRmVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred |
-  Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest |
-  Add-AzureRmVMNetworkInterface -Id $nic.Id
-
-# Create a virtual machine
-New-AzureRmVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig
